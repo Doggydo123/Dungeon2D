c4ca449f52b232b51f99e183e15792071959431f
Dominic Stufkens <stufkensd@gmail.com>
2024-04-26 12:51:27 +1200

    Added dynamic generation of rooms

--- Dungeon/Assets/Scripts/RoomGeneration/RoomLoader.cs
+++ Dungeon/Assets/Scripts/RoomGeneration/RoomLoader.cs
@@ -6,22 +6,38 @@ public class RoomLoader : MonoBehaviour
     public GameObject StartRoom;
     public GameObject roomTemplate; // The room template
 
-    public int AmountOfRooms = 1;
+    public int AmountOfRooms = 6;
     private GameObject currentRoom;
     void Start()
     {
+        System.Random rnd = new System.Random();
         currentRoom = StartRoom;
+        int previous = -1;
         for(int i = 0; i<AmountOfRooms; i++){
-            LoadRooms("left");
+            int nextRoom = rnd.Next(0,4);
+            while(nextRoom == previous){
+                nextRoom = rnd.Next(0,4);
+            }
+            LoadRooms(nextRoom);
+            if(nextRoom == 0){
+                previous = 1;
+            }
+            if(nextRoom == 1){
+                previous = 0;
+            }else if(nextRoom == 2){
+                previous = 3;
+            }else if(nextRoom ==3){
+                previous = 2;
+            }
         }
     }
 
-    void LoadRooms(string side)
+    void LoadRooms(int side)
     {
         GameObject newRoom = null;
 
         // left case
-        if (side == "left")
+        if (side == 0)
         {
             Vector3 startPosition = currentRoom.transform.position;
             Vector3 roomPosition = startPosition + new Vector3(-3.75502f, 0f, 0f); // Adjust the x-coordinate as per your room size
@@ -29,7 +45,7 @@ public class RoomLoader : MonoBehaviour
         }
 
         // right case
-        if (side == "right")
+        if (side == 1)
         {
             Vector3 startPosition = currentRoom.transform.position;
             Vector3 roomPosition = startPosition + new Vector3(3.75502f, 0f, 0f); // Adjust the x-coordinate as per your room size
@@ -37,7 +53,7 @@ public class RoomLoader : MonoBehaviour
         }
 
         // top case
-        if (side == "top")
+        if (side == 2)
         {
             Vector3 startPosition = currentRoom.transform.position;
             Vector3 roomPosition = startPosition + new Vector3(0f, 1.9708f, 0f); // Adjust the y-coordinate as per your room size
@@ -45,10 +61,10 @@ public class RoomLoader : MonoBehaviour
         }
 
         // bottom case
-        if (side == "bottom")
+        if (side == 3)
         {
             Vector3 startPosition = currentRoom.transform.position;
-            Vector3 roomPosition = startPosition + new Vector3(0f, -1.9708f, -10f); // Adjust the y-coordinate as per your room size
+            Vector3 roomPosition = startPosition + new Vector3(0f, -1.9708f, 0f); // Adjust the y-coordinate as per your room size
             newRoom = Instantiate(roomTemplate, roomPosition, Quaternion.identity, transform);
         }
 
