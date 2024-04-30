b14b3a3d9027f585993e8f24b8e3c60fb3b9e06c
Dominic Stufkens <stufkensd@gmail.com>
2024-04-30 17:20:45 +1200

    Refinements

--- Dungeon/Assets/Scripts/CharacterScripts/CharacterMove.cs
+++ Dungeon/Assets/Scripts/CharacterScripts/CharacterMove.cs
@@ -1,10 +1,14 @@
+using System;
+using System.Linq;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f; // Adjust this to change movement speed
     public int stepsAfterCollision = 5; // Adjust this to change the number of steps to walk after collision
 
+    public float radiusCheck = 1;
     private Rigidbody2D rb;
     private Animator myAnimator;
 
@@ -24,58 +28,73 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        //Debug.Log("Sanity");
         // Check if any option box is active
         if (!OptionBoxManager.isOptionBoxOpen)
         {
-            if (remainingSteps > 0)
+            // Get input from the player
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            int[] wall = CheckForColliders();
+            //top
+            if(wall.Length > 0){
+                if(wall.Contains(0)){
+                    if(moveVertical > (float)0){
+                        moveVertical = 0;
+                    }
+                    //bottom
+                }else if(wall.Contains(1)){
+                    if(moveVertical < (float)0){
+                        moveVertical = 0;
+                    }
+                    //right
+                }
+                
+                if(wall.Contains(2)){
+                    if(moveHorizontal > (float)0){
+                        moveHorizontal = 0;
+                    }
+                    //left
+                }else if(wall.Contains(3)){
+                    if(moveHorizontal < (float)0){
+                        moveHorizontal = 0;
+                    }
+                }
+            }
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+            // Move the player
+            rb.velocity = movement * speed;
+
+            // Determine the movement direction
+            left = moveHorizontal < 0;
+            right = moveHorizontal > 0;
+            up = moveVertical > 0;
+            down = moveVertical < 0;
+            idle = !left && !right && !up && !down;
+
+            // Set animator parameters based on movement direction
+            if (left)
             {
-                // Continue walking in the current direction
-                Debug.Log("Moving");
-                MoveCharacter();
-                remainingSteps--;
+                myAnimator.Play("RobotLeft");
+            }
+            else if (right)
+            {
+                myAnimator.Play("RobotRight");
+            }
+            else if (up)
+            {
+                myAnimator.Play("RobotUp");
+            }
+            else if (down)
+            {
+                myAnimator.Play("RobotDown");
             }
             else
             {
-                // Get input from the player
-                float moveHorizontal = Input.GetAxis("Horizontal");
-                float moveVertical = Input.GetAxis("Vertical");
-
-                // Calculate the movement direction
-                Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-
-                // Move the player
-                rb.velocity = movement * speed;
-
-                // Determine the movement direction
-                left = moveHorizontal < 0;
-                right = moveHorizontal > 0;
-                up = moveVertical > 0;
-                down = moveVertical < 0;
-                idle = !left && !right && !up && !down;
-
-                // Set animator parameters based on movement direction
-                if (left)
-                {
-                    myAnimator.Play("RobotLeft");
-                }
-                else if (right)
-                {
-                    myAnimator.Play("RobotRight");
-                }
-                else if (up)
-                {
-                    myAnimator.Play("RobotUp");
-                }
-                else if (down)
-                {
-                    myAnimator.Play("RobotDown");
-                }
-                else
-                {
-                    myAnimator.Play("RobotIdle");
-                }
+                myAnimator.Play("RobotIdle");
             }
+            
+            // Check for colliders here
+
         }
         else
         {
@@ -85,21 +104,36 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    int[] CheckForColliders()
     {
-        Debug.Log("Collision  detected!");
-        // If collided with the room change collider, start walking for the specified number of steps
-        if (other.gameObject.CompareTag("RoomChangeCollider"))
+        // Example of checking for colliders using OverlapCircle
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, (float).01);
+        List<int> walls = new List<int>();
+        foreach (Collider2D collider in colliders)
         {
-            Debug.Log("Collision with RoomChangeCollider detected!");
-            remainingSteps = stepsAfterCollision;
+            // Check for specific tags or layers if needed
+            if (collider.CompareTag("TopWall"))
+            {
+                walls.Add(0);
+            }
+            else if (collider.CompareTag("BotWall"))
+            {
+                // Handle collision with item
+                walls.Add(1);
+            }
+            if (collider.CompareTag("RightWall"))
+            {
+                // Handle collision with item
+                walls.Add(2);
+            }
+            else if (collider.CompareTag("LeftWall"))
+            {
+                // Handle collision with item
+                walls.Add(3);
+            }
         }
+        return walls.ToArray();
     }
 
-    private void MoveCharacter()
-    {
-        // Move the player in the direction they were previously moving
-        Vector2 movement = new Vector2(left ? -1 : (right ? 1 : 0), up ? 1 : (down ? -1 : 0));
-        rb.velocity = movement * speed;
-    }
+
 }
---
+++ Dungeon/Assets/Scripts/RoomGeneration/RoomVariables.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomVariables : MonoBehaviour
+{
+public bool left;
+public bool right;
+public bool top;
+public bool bottom;
+}
